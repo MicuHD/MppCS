039188d8d52e3b4b8e86896738ea12e0fa0fc126
Claudiu Pop <micu_hd@yahoo.com>
2017-03-26 13:19:47 +0300

    Validate input data

--- labMpp/GUI/MeniuView.cs
+++ labMpp/GUI/MeniuView.cs
@@ -15,6 +15,11 @@ namespace labMpp.GUI
 {
     public partial class MeniuView : Form, Utils.IObserver<Spectacol>
     {
+        enum Tabel{
+              SpectacolTabel,
+              CautareTabel 
+        }
+
         private CommandService service;
         private BindingSource source;
         private BindingSource cautaSource;
@@ -41,7 +46,7 @@ namespace labMpp.GUI
             cautaTable.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
 
             cautaTable.DataSource = cautaSource;
-            setColor();
+            setColor(Tabel.SpectacolTabel);
 
         }
 
@@ -52,15 +57,29 @@ namespace labMpp.GUI
 
         }
 
-        private void setColor()
+        private void setColor(Tabel tab)
         {
-            foreach (DataGridViewRow row in spectacolTable.Rows)
+            if(tab.Equals(Tabel.SpectacolTabel))
             {
-                if (row.Cells[2].Value.Equals(0))
+                foreach (DataGridViewRow row in spectacolTable.Rows)
                 {
-                    row.DefaultCellStyle.BackColor = Color.Red;
+                    if (row.Cells[2].Value.Equals(0))
+                    {
+                        row.DefaultCellStyle.BackColor = Color.Red;
+                    }
                 }
             }
+            else
+            {
+                foreach (DataGridViewRow row in cautaTable.Rows)
+                {
+                    if (row.Cells[2].Value.Equals(0))
+                    {
+                        row.DefaultCellStyle.BackColor = Color.Red;
+                    }
+                }
+            }
+            
         }
 
 
@@ -72,7 +91,7 @@ namespace labMpp.GUI
             spectacolTable.DataSource = source;
             cautaText.Text = "";
             cautaTable.DataSource = null;
-            setColor();
+            setColor(Tabel.CautareTabel);
         }
 
         private void logoutBtn_Click(object sender, EventArgs e)
@@ -82,19 +101,66 @@ namespace labMpp.GUI
 
         private void cautaBtn_Click(object sender, EventArgs e)
         {
-            List<Spectacol> cautaList = service.cautare(cautaText.Text);
-            cautaSource = new BindingSource(new BindingList<Spectacol>(cautaList), null);
-            cautaTable.DataSource = cautaSource;
+            string data = cautaText.Text;
+            string[] items = data.Split('-');
+
+            if(items.Count().Equals(3) && items[0].Count().Equals(2) && items[1].Count().Equals(2) && items[2].Count().Equals(4))
+            {
+                List<Spectacol> cautaList = service.cautare(cautaText.Text);
+                cautaSource = new BindingSource(new BindingList<Spectacol>(cautaList), null);
+                cautaTable.DataSource = cautaSource;
+                setColor(Tabel.CautareTabel);
+            }
+            else
+            {
+                MessageBox.Show("Introduceti o data valida!");
+            }
         }
 
         private void vanzareBtn_Click(object sender, EventArgs e)
         {
-            int id = (int)cautaTable.SelectedRows[0].Cells[0].Value;
-            Console.Out.WriteLine("id = " + id.ToString());
-            Spectacol spec = service.findSpectacol(id);
-            Console.Out.WriteLine("spec id = " + spec.Id.ToString());
+            try
+            {
+                int id = (int)cautaTable.SelectedRows[0].Cells[0].Value;
+                Console.Out.WriteLine("id = " + id.ToString());
+                Spectacol spec = service.findSpectacol(id);
+                Console.Out.WriteLine("spec id = " + spec.Id.ToString());
+                string nume = numeText.Text;
+                char[] cifre = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
 
-            service.cumparare(spec, numeText.Text, int.Parse(bileteText.Text));
+                int tickets;
+                bool ok = int.TryParse(bileteText.Text, out tickets);
+
+                if(!cifre.Any(s => nume.Contains(s)))
+                {
+                    if (ok && tickets > 0)
+                    {
+                        if (tickets <= spec.Disponibile)
+                        {
+                            service.cumparare(spec, numeText.Text, int.Parse(bileteText.Text));
+                        }
+                        else
+                        {
+                            MessageBox.Show("Numarul de bilete cerut nu este disponibil");
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Numarul de bilete trebuie sa fie un numar intreg pozitiv");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Numele nu poate contine cifre");
+                }
+                
+            }
+            catch(System.ArgumentOutOfRangeException ex)
+            {
+                MessageBox.Show("Alegeti un spectacol din lista de cautare");
+            }
+            
         }
     }
 }
